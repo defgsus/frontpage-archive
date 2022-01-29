cdc7d77b200b961da4f559e96c609dd84eb660e9
bergi <s.berke@netzkolchose.de>
2022-01-29 13:33:39 +0100

    add faz.net

--- src/sources/__init__.py
+++ src/sources/__init__.py
@@ -1,5 +1,6 @@
 from .bild import Bild
 from .compact import Compact
+from .faz import FrankfurterAllgemeine
 from .fr import FrankfurterRundschau
 from .heise import Heise
 from .spiegel import Spiegel, SpiegelDaily
