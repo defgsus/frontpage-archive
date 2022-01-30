9a249c69f693e4aba2ed2a5649019902b375553e
bergi <s.berke@netzkolchose.de>
2022-01-30 18:28:23 +0100

    stash the article iterator for compact-online.de

--- src/sources/press/compact.py
+++ src/sources/press/compact.py
@@ -1,3 +1,4 @@
+from typing import Generator
 from ...scraper import Scraper
 
 
@@ -14,3 +15,17 @@ class Compact(Scraper):
         ("impressum.html", URL + "rechtliches/impressum/"),
         ("datenschutz.html", URL + "rechtliches/datenschutz/"),
     ]
+
+    def iter_articles(self, filename: str, content: str) -> Generator[dict, None, None]:
+        soup = self.to_soup(content)
+        for tag in soup.find_all("div", {"class": "content"}):
+            if not (tag.text and tag.text.strip()):
+                continue
+
+            article = self.create_article_dict(
+                title=self.strip(tag.find("div", {"class": "post-meta"})),
+                teaser=self.strip(tag.find("div", {"class": "excerpt"}))
+            )
+
+            yield article
+            
